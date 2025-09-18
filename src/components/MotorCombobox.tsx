06cb83b44af404ac83e73903d9f37047824a14ec
LittlePisi <50640480+LittlePisi@users.noreply.github.com>
2025-09-18 12:41:52 +0300

    Update MotorCombobox.tsx

--- src/components/MotorCombobox.tsx
+++ src/components/MotorCombobox.tsx
@@ -93,7 +93,7 @@ const MTComboBox: React.FC<MTComboBoxProps> = ({
         onChange={(e) => handleChange(e.target.value)}
         className={`mt-0 px-3 block w-full h-9 text-sm rounded-lg shadow-sm focus:border-blue-400 focus:ring-blue-300 sm:text-sm ${
           darkMode ? 'bg-gray-700 border border-gray-600 text-gray-200 focus:border-gray-500'
-                   : ' border border-gray-300 text-gray-700 focus:border-blue-500'
+                   : ' bg-gray-100 border border-gray-300 text-gray-700 focus:border-blue-500'
         }`}
       >
         {parameterSets.map((set) => (
